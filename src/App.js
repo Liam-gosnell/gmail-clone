1862926228e668eb4aa93520a0c71f79e584be0b
Liam-gosnell <liamgoz99@gmail.com>
2021-10-20 15:36:48 +0100

    Added Header.js/css, Sidebar.js/css & packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h2>Lets build gmail</h2>
+      <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './Sidebar.css';
+import AddIcon from "@material-ui/icons/Add";
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <Button startIcon={<AddIcon fontSize="large" />}>
+                Compose
+            </Button>
+        </div>
+    )
+}
+
+export default Sidebar
