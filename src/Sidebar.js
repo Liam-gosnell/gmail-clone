2ccb31572f03530469ed4de052349c9652b3bfde
Liam-gosnell <liamgoz99@gmail.com>
2021-10-20 16:14:40 +0100

    Added SidebarOption

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,14 +2,20 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import './Sidebar.css';
 import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from './Sidebar';
 
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <Button startIcon={<AddIcon fontSize="large" />}>
+            <Button startIcon={<AddIcon fontSize="large"/>}
+            className="sidebar__compose"
+            >
                 Compose
             </Button>
+
+            <SidebarOption Icon={InboxIcon} title="inbox" number={65}/>
         </div>
     )
 }
