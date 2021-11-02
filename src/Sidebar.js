5386ebf279f527e763ddfde1be01aa6dc566466e
Liam-gosnell <liamgoz99@gmail.com>
2021-11-02 21:33:27 +0000

    Added all sidebar options with icons imported

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,11 @@ import './Sidebar.css';
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import SidebarOption from './SidebarOption';
 
 
@@ -18,6 +23,11 @@ function Sidebar() {
 
             <SidebarOption Icon={InboxIcon} title="inbox" number={65} selected={true}/>
             <SidebarOption Icon={StarIcon} title="Starred" number={65}/>
+            <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={65}/>
+            <SidebarOption Icon={LabelImportantIcon} title="Important" number={65}/>
+            <SidebarOption Icon={NearMeIcon} title="Sent" number={65}/>
+            <SidebarOption Icon={NoteIcon} title="Drafts" number={65}/>
+            <SidebarOption Icon={ExpandMoreIcon} title="More" number={65}/>
         </div>
     )
 }
