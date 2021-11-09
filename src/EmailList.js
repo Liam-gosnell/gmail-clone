7daa2e7a0e4dfd2066255540bb350d73ba78930c
Liam-gosnell <liamgoz99@gmail.com>
2021-11-09 20:09:03 +0000

    added icons to emailList & styles

--- src/EmailList.js
+++ src/EmailList.js
@@ -3,6 +3,10 @@ import { Checkbox, IconButton } from "@material-ui/core"
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 import './EmailList.css'; 
 
@@ -22,6 +26,23 @@ function EmailList() {
                         <MoreVertIcon />
                     </IconButton>
                 </div>
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div> 
+            </div>
+            <div className="emailList__sections">
+                
             </div>
         </div>
     );
