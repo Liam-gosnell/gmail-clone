57ead2c789ceaff1928488f7891be9d45cf2eceb
Liam-gosnell <liamgoz99@gmail.com>
2021-11-11 16:04:40 +0000

    Added Icons to Mail.js page

--- src/Mail.js
+++ src/Mail.js
@@ -1,10 +1,62 @@
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
+import ErrorIcon from '@material-ui/icons/Error'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EmailIcon from '@material-ui/icons/Email'
+import WatchLaterIcon from '@material-ui/icons/WatchLater'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
+import LabelImportantIcon from '@material-ui/icons/LabelImportant'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 import React from 'react';
+import { IconButton } from '@material-ui/core';
 import './Mail.css';
 
 function Mail() {
     return (
         <div className="mail">
-            
+            <div className="mail__tools">
+                <div className="mail__toolsLeft">
+                    <IconButton>
+                        <ArrowBackIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <MoveToInboxIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <ErrorIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <DeleteIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <EmailIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <WatchLaterIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <CheckCircleIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <LabelImportantIcon/>
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+                <div className="mail_toolsRight">
+
+                </div>
+            </div>
+            <h1>...</h1>
         </div>
     )
 }
