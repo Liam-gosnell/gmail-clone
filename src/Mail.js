abd4b995c428aafbbfcacf807b47ea98198ebab6
Liam-gosnell <liamgoz99@gmail.com>
2021-11-14 15:09:31 +0000

    Added ExitToAppIcon & print buttons

--- src/Mail.js
+++ src/Mail.js
@@ -1,13 +1,15 @@
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
-import ErrorIcon from '@material-ui/icons/Error'
-import DeleteIcon from '@material-ui/icons/Delete'
-import EmailIcon from '@material-ui/icons/Email'
-import WatchLaterIcon from '@material-ui/icons/WatchLater'
-import CheckCircleIcon from '@material-ui/icons/CheckCircle'
-import LabelImportantIcon from '@material-ui/icons/LabelImportant'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore'
+import ErrorIcon from '@material-ui/icons/Error';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EmailIcon from '@material-ui/icons/Email';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore';
+import PrintIcon from '@material-ui/icons/Print';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import React from 'react';
 import { IconButton } from '@material-ui/core';
 import './Mail.css';
@@ -64,11 +66,11 @@ function Mail() {
                     </IconButton>
 
                     <IconButton>
-                        <MoreVertIcon/>
+                        <PrintIcon/>
                     </IconButton>
 
                     <IconButton>
-                        <MoreVertIcon/>
+                        <ExitToAppIcon/>
                     </IconButton>
                 </div>
             </div>
