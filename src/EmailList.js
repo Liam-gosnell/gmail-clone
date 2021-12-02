6bfea471eb7bd9fd20205a7d9ab75a8b1ff010f8
Liam-gosnell <liamgoz99@gmail.com>
2021-12-02 19:31:14 +0000

    Added map function to render the real time email submissions from firebase

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Checkbox, IconButton } from "@material-ui/core"
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
@@ -14,8 +14,20 @@ import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import './EmailList.css'; 
 import Section from './Section';
 import EmailRow from './EmailRow';
+import { db } from './firebase';
 
 function EmailList() {
+
+    const [emails, setEmails] = useState([]);
+
+    useEffect (() => {
+        db.collection('emails').orderBy('timestamp', 'desc').onSnapshot
+        (snapshot => setEmails(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data(),
+        }))))
+    }, []) 
+
     return (
         <div className="emailList">
             <div className="emailList__settings">
@@ -52,6 +64,17 @@ function EmailList() {
                 <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
             </div>
             <div className="emailList__list">
+                {emails.map(({id, data: { to, subject, message, timestamp }}) => (
+                    <EmailRow 
+                        id={id}
+                        key={id}
+                        title={to}
+                        subject={subject}
+                        description={message}
+                        time={new Date(timestamp?.seconds * 1000).toUTCString()}
+                    
+                    />
+                ))}
                 <EmailRow 
                     title="LinkedIn"
                     subject="Job notification"
